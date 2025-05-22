a07688182acfae348ff1dbe9a57698240048db68
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2025-05-22 12:48:43 +0800

    chore: add utilities

--- index.php
+++ index.php
@@ -5,7 +5,13 @@ declare(strict_types=1);
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Database\Dialects\SQLiteDB;
+use Database\Utils;
 
 $database = SQLiteDB::getInstance();
+$dbUtils = new Utils();
+
+echo $dbUtils->getSqliteVersion($database);
+echo "\n";
+echo $dbUtils->checkIfTableExists($database, 'todos');
 
 die();
