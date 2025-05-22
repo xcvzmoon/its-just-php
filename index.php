11b53dd24678c8f185f020ce7e13ca4536cd0de5
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2025-05-22 13:45:46 +0800

    chore: add testing

--- index.php
+++ index.php
@@ -6,12 +6,36 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Database\Dialects\SQLiteDB;
 use Database\Utils;
+use Models\Flight;
+
+echo "\n";
 
 $database = SQLiteDB::getInstance();
 $dbUtils = new Utils();
 
-echo $dbUtils->getSqliteVersion($database);
+echo "{$dbUtils->getSqliteVersion($database)} \n";
+echo "{$dbUtils->checkIfTableExists($database, 'flights')} \n";
 echo "\n";
-echo $dbUtils->checkIfTableExists($database, 'todos');
 
-die();
+try {
+    echo "TESTING: \n";
+
+    $flight = new Flight(
+        "AA123",
+        "LAX",
+        "JFK",
+        "2025-06-01 08:00:00",
+        "2025-06-01 16:30:00",
+        "American Airlines"
+    );
+
+    echo "Initial flight: $flight \n";
+
+    $flight->setDepartureTime("2025-06-01 09:00:00");
+
+    echo "Updated flight: $flight \n";
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage() . "\n";
+}
+
+die("\n");
